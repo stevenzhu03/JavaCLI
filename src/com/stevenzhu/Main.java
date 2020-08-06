45421efeb2a050b3e2719a00837a4e8d53ee8dc3
Steven Zhu <stevenzhu03@gmail.com>
2020-08-06 17:11:52 -0400

    refactured mortgage reports to MortgageReport class

--- src/com/stevenzhu/Main.java
+++ src/com/stevenzhu/Main.java
@@ -1,7 +1,5 @@
 package com.stevenzhu;
 
-import java.text.NumberFormat;
-
 public class Main {
 
   final static byte MONTHS_IN_YEAR = 12;
@@ -13,50 +11,9 @@ public class Main {
     byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
     MortgageReport.printMortgage(principal, annualInterest, years);
-    printPaymentSchedule(principal, annualInterest, years);
+    MortgageReport.printPaymentSchedule(principal, annualInterest, years);
   }
 
-  private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
-    System.out.println();
-    System.out.println("PAYMENT SCHEDULE");
-    System.out.println("----------------");
-    for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
-      double balance = calculateBalance(principal, annualInterest, years, month);
-      System.out.println(NumberFormat.getCurrencyInstance().format(balance));
-    }
-  }
-
-  public static double calculateBalance(
-      int principal,
-      float annualInterest,
-      byte years,
-      short numberOfPaymentsMade
-  ) {
-    float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-    float numberOfPayments = years * MONTHS_IN_YEAR;
-
-    double balance = principal
-        * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math
-        .pow(1 + monthlyInterest, numberOfPaymentsMade))
-        / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-
-    return balance;
-  }
-
-  public static double calculateMortgage(
-      int principal,
-      float annualInterest,
-      byte years) {
-
-    float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-    float numberOfPayments = years * MONTHS_IN_YEAR;
-
-    double mortgage = principal
-        * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
-        / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-
-    return mortgage;
-  }
 }
 
 
