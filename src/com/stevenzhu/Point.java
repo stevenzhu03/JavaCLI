887dbba97f83f88ad1fa37b0618daeed337105af
Steven Zhu <stevenzhu03@gmail.com>
2020-08-10 16:49:45 -0400

    override equals method in point class

--- src/com/stevenzhu/Point.java
+++ src/com/stevenzhu/Point.java
@@ -1,5 +1,7 @@
 package com.stevenzhu;
 
+import java.util.Objects;
+
 public class Point {
   private int x;
   private int y;
@@ -9,15 +11,39 @@ public class Point {
     this.y = y;
   }
 
+//  @Override
+//  public boolean equals(Object obj) {
+//    if (this == obj)
+//      return true;
+//
+//    if (!(obj instanceof Point))
+//      return false;
+//
+//    var other = (Point) obj;
+//    return other.x == x && other.y == y;
+//  }
+//
+//  @Override
+//  public int hashCode() {
+//    return Objects.hash(x, y);
+//  }
+
+
   @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
+  public boolean equals(Object o) {
+    if (this == o) {
       return true;
-
-    if (!(obj instanceof Point))
+    }
+    if (o == null || getClass() != o.getClass()) {
       return false;
+    }
+    Point point = (Point) o;
+    return x == point.x &&
+        y == point.y;
+  }
 
-    var other = (Point) obj;
-    return other.x == x && other.y == y;
+  @Override
+  public int hashCode() {
+    return Objects.hash(x, y);
   }
 }
